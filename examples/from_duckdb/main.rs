53cf2203ccf196f497434c733e19f0da253b2a3f
angelip2303 <angel.iglesias.prestamo@gmail.com>
2023-04-13 23:20:04 +0200

    removing duckdb from this repo

--- examples/from_duckdb/main.rs
+++
@@ -1,9 +0,0 @@
-use pregel_rs::graph_frame::GraphFrame;
-use std::error::Error;
-use std::path::Path;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let connection = duckdb::Connection::open(Path::new("./examples/from_duckdb/example.duckdb"))?;
-    let dataframe = GraphFrame::from_duckdb(connection)?;
-    Ok(println!("{}", dataframe))
-}
